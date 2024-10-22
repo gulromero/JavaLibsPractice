3dfd7a20eccb4d64a581dfa6476411f0e2947b4d
Gülsüm <gulsumerd38@gmail.com>
2024-10-22 10:47:08 +0800

    im in class rn hehe adding some stuff the professor mentioned

--- JavaLibsPractice.java
+++ JavaLibsPractice.java
@@ -5,11 +5,13 @@ public class JavaLibsPractice {
     public static void main(String[] args) {
 
         //application logic here
+        //Customize the InputDialog (title, the placement text etc)
+        //ShowMessageDialog doesn't provide a field for input (done)
         String name = JOptionPane.showInputDialog("Hi! Whats your name?");
         int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you?"));
         double price = Double.parseDouble(JOptionPane.showInputDialog("Enter a random number"));
         String noun = JOptionPane.showInputDialog("Enter a noun");
-
+    
         // Perfoming calculations for age and price
         int nextYear = age + 1;
         double randomNumber2 = price * 100;
@@ -19,6 +21,10 @@ public class JavaLibsPractice {
        
         // Displaying the story
         JOptionPane.showMessageDialog(null, story);
+
+        //Show message that game is done, without input field
+        JOptionPane.showMessageDialog(null,"MadLibs game is now done!");
+
 
     }   
 }

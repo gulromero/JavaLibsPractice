7eb39baa5a8afeb873d328ef005fe38f8e21abed
Gülsüm <gulsumerd38@gmail.com>
2024-10-22 10:12:38 +0800

    .

--- JavaLibsPractice.java
+++ JavaLibsPractice.java
@@ -3,7 +3,8 @@ import javax.swing.JOptionPane;
 public class JavaLibsPractice {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        //application logic here
         String name = JOptionPane.showInputDialog("Hi! Whats your name?");
         int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you?"));
         double price = Double.parseDouble(JOptionPane.showInputDialog("Enter a random number"));
@@ -11,10 +12,11 @@ public class JavaLibsPractice {
 
         // Perfoming calculations for age and price
         int nextYear = age + 1;
-        double randomNumber2 = price * 10;
+        double randomNumber2 = price * 100;
 
         // Creating a lil' story
         String story = "Hi! My name is " + name + " I am " + age + " years old." + " In some years, I'll be " + nextYear + " years old. " + " I bought a " + noun + " for " + price + " but atleast it wasn't " + randomNumber2;
+       
         // Displaying the story
         JOptionPane.showMessageDialog(null, story);
 
